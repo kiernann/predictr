7bdd3d93449eabdc858e4348cad114a2d309ad75
Kiernan Nicholls <kiernann@protonmail.com>
2020-07-16 10:03:09 -0400

    Fix tests for new format

--- tests/testthat/test-market-history.R
+++ tests/testthat/test-market-history.R
@@ -3,21 +3,8 @@ library(predictr)
 
 test_that("discrete market price returns", {
   x <- market_history(mid = 3633)
-  expect_length(x, 10)
+  expect_length(x, 8)
   expect_s3_class(x, "tbl")
   expect_length(unique(x$mid), 1)
-})
-
-test_that("percentage interval market price returns with factors", {
-  x <- market_history(mid = 6360, hourly = FALSE)
-  expect_length(x, 10)
-  expect_s3_class(x, "tbl")
-  expect_s3_class(x$time, "Date")
-})
-
-test_that("hourly price history can be returned", {
-  x <- market_history(mid = 5241, hourly = TRUE)
-  expect_length(x, 10)
-  expect_s3_class(x, "tbl")
   expect_s3_class(x$time, "POSIXct")
 })
--- tests/testthat/test-market-price.R
+++ tests/testthat/test-market-price.R
@@ -3,18 +3,8 @@ library(predictr)
 
 test_that("discrete market price returns", {
   dat <- market_price(mid = 3633)
-  expect_length(dat, 7)
-  expect_s3_class(dat, "tbl")
-})
-
-test_that("percentage interval market price returns with factors", {
-  dat <- market_price(mid = 6360)
-  expect_length(dat, 7)
-  expect_s3_class(dat, "tbl")
-})
-
-test_that("integer interval market price returns with end date", {
-  dat <- market_price(mid = 6400)
   expect_length(dat, 8)
   expect_s3_class(dat, "tbl")
+  expect_s3_class(dat$time, "POSIXct")
+  expect_s3_class(dat$end, "POSIXct")
 })
